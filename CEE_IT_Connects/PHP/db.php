f5bf1e1a0d07597a8407baad5732d92bfb820f6e
JajaTheFatFuckingHut <jamesherold25@gmail.com>
2026-09-13 14:39:07 +0800

    Change in db.php

--- CEE_IT_Connects/PHP/db.php
+++ CEE_IT_Connects/PHP/db.php
@@ -7,12 +7,26 @@ if (!$databaseUrl) {
 }
 
 try {
-    $pdo = new PDO($databaseUrl);
+    $db = parse_url($databaseUrl);
+
+    if ($db === false) {
+        throw new Exception("Invalid DATABASE_URL.");
+    }
+
+    $host = $db['host'];
+    $port = $db['port'] ?? 5432;
+    $dbname = ltrim($db['path'], '/');
+    $user = $db['user'];
+    $password = $db['pass'];
+
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+
+    $pdo = new PDO($dsn, $user, $password);
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-} catch (PDOException $e) {
+} catch (Exception $e) {
     die("Database connection failed: " . $e->getMessage());
 }
 ?>
