abe6b75b708c2abba4d35fba9e4e7788b6e10d7b
JajaTheFatFuckingHut <jamesherold25@gmail.com>
2026-09-13 15:21:37 +0800

    Css

--- CEE_IT_Connects/PHP/login-ui.php
+++ CEE_IT_Connects/PHP/login-ui.php
@@ -15,7 +15,7 @@ $registerVisible = file_exists($statePath) ? trim(file_get_contents($statePath))
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" href="../student-login.css">
+    <link rel="stylesheet" href="student-login.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
 </head>
 
