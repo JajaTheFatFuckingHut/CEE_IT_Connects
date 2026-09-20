0b6d9a55a0963ae63fb42e61c7d9a443a134d9ca
JajaTheFatFuckingHut <jamesherold25@gmail.com>
2026-09-20 15:49:41 +0800

    ATTEMPT 7

--- CEE_IT_Connects/PHP/applied-Internship-programs.php
+++ CEE_IT_Connects/PHP/applied-Internship-programs.php
@@ -85,9 +85,8 @@ foreach ($internships as $loc) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="stylesheet" href="../CSS/index-style.css">
-    <link
-        href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;1,9..40,300&family=Fraunces:ital,opsz,wght@0,9..144,300;0,9..144,600;1,9..144,300&display=swap"
-        rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" crossorigin="">
+
 
     <style>
         :root {
@@ -1286,8 +1285,7 @@ foreach ($internships as $loc) {
             </div>
         </div>
     </section>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.js"></script>
-
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" crossorigin=""></script>
     <script>
         function togglePanel(id, btn) {
             const card = btn.closest('.listing-card');
