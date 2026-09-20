5c77fc2c5d955ea4f3e738f75cbec96a928824df
JajaTheFatFuckingHut <jamesherold25@gmail.com>
2026-09-21 01:06:12 +0800

    ATTEMPT 6

--- CEE_IT_Connects/PHP/superadmin.php
+++ CEE_IT_Connects/PHP/superadmin.php
@@ -1515,7 +1515,7 @@ $programHoursList = $programHoursStmt->fetchAll(PDO::FETCH_ASSOC);
                                     <td>
                                         <form method="POST" action="superadmin-db.php"
                                             onsubmit="return confirm('Are you sure you want to delete this user?')">
-                                            <input type="hidden" name="id" value="<?= $u['id'] ?>">
+                                            <input type="hidden" name="user_id" value="<?= $u['id'] ?>">
                                             <input type="hidden" name="source" value="<?= $u['source'] ?>">
                                             <button type="submit" name="delete" class="btn-delete">
                                                 <i class="bi bi-trash-fill"></i> Delete
